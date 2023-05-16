cb3ee8f7ce685e90c4d9bf01ca815b9304e86a69
Gustavohaa <111452073+Gustavohaa@users.noreply.github.com>
2023-05-16 17:38:12 -0300

    Add files via upload

---
+++ src/Progam/Principal.java
@@ -0,0 +1,33 @@
+package Progam;
+
+import entities.ContaCorrente;
+
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        ContaCorrente conta = new ContaCorrente("Gustavo", "1234567890", "senha");
+
+        conta.depositar(500.0);
+        System.out.println("Depósito realizado. Saldo atual: " + conta.getSaldo());
+
+        conta.sacar(200.0);
+        System.out.println("Saque realizado. Saldo atual: " + conta.getSaldo());
+
+
+        conta.transferirPix("gustavo@gmail.com", 100.0);
+        System.out.println("Transferência via Pix realizada. Saldo atual: " + conta.getSaldo());
+
+
+        conta.transferir("12234", 567389, 150.0);
+        System.out.println("Transferência realizada. Saldo atual: " + conta.getSaldo());
+
+
+        ArrayList<String> extrato = conta.getExtrato();
+        System.out.println("Extrato da conta:");
+        for (String operacao : extrato) {
+            System.out.println(operacao);
+        }
+    }
+}
